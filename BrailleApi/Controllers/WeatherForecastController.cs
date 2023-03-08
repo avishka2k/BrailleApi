4739b039ace45622a45c6da37cc2f47d4a942639
avishka2k <avishka2k@gmail.com>
2023-03-08 16:19:00 +0530

    Add project files.

---
+++ BrailleApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace BrailleApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]/api")]
+    public class DotPatternController : ControllerBase
+    {
+        [HttpGet("rectangle/{width}/{height}")]
+        public IActionResult Rectangle(int width, int height)
+        {
+            string dotPattern = "";
+            for (int i = 0; i < height; i++)
+
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    dotPattern += ". ";
+                }
+                dotPattern += "\n";
+            }
+            return Ok(dotPattern);
+        }
+
+        [HttpGet("circle/{radius}/{resolution}")]
+        public IActionResult GetCircle(int radius, int resolution)
+        {
+            string dotPattern = "";
+            double step = 2 * Math.PI / resolution;
+            for (int y = -radius; y <= radius; y++)
+            {
+                for (int x = -radius; x <= radius; x++)
+                {
+                    double distance = Math.Sqrt(x * x + y * y);
+                    if (Math.Abs(distance - radius) < step / 2)
+                    {
+                        dotPattern += ".";
+                    }
+                    else
+                    {
+                        dotPattern += " ";
+                    }
+                }
+                dotPattern += "\n";
+            }
+            return Ok(dotPattern);
+        }
+
+    }
+}
